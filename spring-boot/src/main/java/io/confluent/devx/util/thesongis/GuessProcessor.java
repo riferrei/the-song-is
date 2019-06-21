56f358c8fd4cc0fa298ada806fef52c1cb404490
riferrei <riferrei@riferrei.net>
2019-06-20 20:00:31 -0400

    Fixed the finish of span during record creation

--- spring-boot/src/main/java/io/confluent/devx/util/thesongis/GuessProcessor.java
+++ spring-boot/src/main/java/io/confluent/devx/util/thesongis/GuessProcessor.java
@@ -47,7 +47,9 @@ public class GuessProcessor {
                 new ProducerRecord<String, String>(GUESSES,
                     null, null, value, headers);
 
-            TracingKafkaUtils.buildAndInjectSpan(record, GlobalTracer.get());
+            TracingKafkaUtils.buildAndInjectSpan(
+                record, GlobalTracer.get()).finish();
+
             kafkaTemplate.send(record);
 
         }
